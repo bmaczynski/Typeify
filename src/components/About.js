18d0560a4d901b593de886e3ede96ab70b892ca2
Brandon Maczynski <bmaczynski11@yahoo.com>
2024-01-25 12:11:00 -0500

    text changes

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,8 @@ function About() {
       <div className="bg-neutral-800 max-w-3xl mt-[-136px] rounded-md p-5 text-2xl tracking-wide font-medium">
         Typeify.io is a type test website created by myself to further my
         knowledge in the programming world. I built it to extend my knowledge
-        using React, and also implementing the design I've created. I will be
-        implementing more features as time goes on, and always open to
-        suggestions from the community.
+        using React. I will be implementing more features as time goes on, and
+        always open to suggestions from the community.
       </div>
     </div>
   );
