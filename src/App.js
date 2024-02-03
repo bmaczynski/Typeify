4257bc0d265939e18e9fffbff591e07d570e679c
Brandon Maczynski <bmaczynski11@yahoo.com>
2024-02-03 13:20:20 -0500

    removed auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Auth from "./components/Auth";
 import Main from "./components/Main";
 import "./components/App.css";
 import Nav from "./components/Nav";
@@ -9,12 +8,11 @@ import About from "./components/About";
 function App() {
   return (
     <div className="w-full flex flex-col items-center min-h-screen bg-neutral-900">
-      <div className="flex flex-col max-w-screen-sm">
+      <div className="flex flex-col md:max-w-screen-md max-w-screen-sm">
         <Router>
           <Nav />
           <Routes>
             <Route path="/" element={<Main />} />
-            <Route path="/login" element={<Auth />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </Router>
